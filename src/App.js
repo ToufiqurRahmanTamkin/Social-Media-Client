7e76504c707d39ec1c8dee45a40f139bcf7393ec
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-08-20 01:14:21 +0600

    setup project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 const App = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <div>
-      hello
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
